d20a5d69aa33e76afe52631d79c4ff055fb7d05d
heriwahyudiono <99733430+heriwahyudiono@users.noreply.github.com>
2023-09-27 19:59:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -380,6 +380,6 @@ app.use(function (req, res, next) {
   res.status(404).render("page-not-found");
 });
 
-app.listen(8080, function () {
-  console.log("Server listening on port 8080");
+app.listen(3000, function () {
+  console.log("Server listening on port 3000");
 });
