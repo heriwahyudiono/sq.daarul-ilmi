e79e513bf848e108d040224a847c657051009dd9
heriwahyudiono <heriwhydiono@gmail.com>
2023-10-06 22:22:29 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'sq.daarul-ilmi';
+const urlsToCache = [
+  './assets/images/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['sq.daarul-ilmi'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
