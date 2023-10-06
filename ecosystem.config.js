2ec964cb19c1f882749771f31c9b6ec072075b2e
heriwahyudiono <heriwhydiono@gmail.com>
2023-10-06 19:36:43 +0700

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+      {
+        name: 'sq.daarul-ilmi', // Nama aplikasi
+        script: 'server.js', // Nama file utama aplikasi
+        instances: 1, // Jumlah instance yang akan dijalankan
+        autorestart: true, // Restart otomatis jika aplikasi berhenti
+        watch: false, // Mengaktifkan mode watch untuk memantau perubahan file
+        max_memory_restart: '1G', // Batas memori untuk restart
+        env: {
+          NODE_ENV: 'production', // Variabel lingkungan
+          PORT: 3000, // Port aplikasi
+        },
+        env_production: {
+          NODE_ENV: 'production', // Variabel lingkungan khusus produksi
+          PORT: 80, // Port aplikasi untuk produksi
+        },
+      },
+      // Anda dapat menambahkan aplikasi lain di sini jika diperlukan
+    ],
+  };
+  
